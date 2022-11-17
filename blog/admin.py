38aaf415e2548682bd2bd6c0f70ff0659c1496c4
Suraj Atkari <surajatkari@yahoo.com>
2022-11-17 14:21:20 +0530

    added Class in admin for pre-popullated Slug

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Post, Author, Tag
 # Register your models here.
 
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Tag)
