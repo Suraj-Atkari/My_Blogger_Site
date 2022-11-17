a903dfce61727623b83bcdb2b9d1bb89ba7ab7a0
Suraj Atkari <surajatkari@yahoo.com>
2022-11-17 11:54:31 +0530

    added Post Model

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Author(models.Model):
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
     email = models.models.EmailField(max_length=254)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=1000)
+    image_name = models.ImageField
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField
+    content = models.CharField(max_length=5000)
