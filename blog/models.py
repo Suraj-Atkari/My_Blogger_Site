eeee5b87cf646a3540014b086dfade9adcdb17e2
Suraj Atkari <surajatkari@yahoo.com>
2022-11-17 14:01:45 +0530

    edit Tag and ManyToMany relations

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,10 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
@@ -18,13 +22,11 @@ class Author(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, default=None)
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, default=None, related_name="posts")
     excerpt = models.CharField(max_length=1000)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-
-
-class Tag(models.Model):
-    caption = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag)
