9969698c7555188c0548aab4ad4300b04289eefd
Suraj Atkari <surajatkari@yahoo.com>
2022-11-17 12:10:25 +0530

    added __str__ for author

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,12 @@ class Author(models.Model):
     last_name = models.CharField(max_length=55)
     email = models.EmailField(max_length=254)
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
