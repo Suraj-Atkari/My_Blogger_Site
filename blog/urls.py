23f6782bfc57004b260f5b5ce95b4888fd0f2c2a
Suraj Atkari <surajatkari@yahoo.com>
2022-11-23 15:06:10 +0530

    add static in urls.py and link with settings.py in main root

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls import static
+from django.conf import settings
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail,
          name="post-detail-page"),  # /posts/my-first-post
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
