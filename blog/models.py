e1f61bc7b51e79b79d97e5e56474482d05bfa485
Suraj Atkari <surajatkari@yahoo.com>
2022-11-23 14:19:20 +0530

    change model for ImageField and added MEDIA ROOT and URL in settings

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, null=True, default=None, related_name="posts")
     excerpt = models.CharField(max_length=1000)
-    image_name = models.CharField(max_length=100, default=None)
+    image = models.ImageField(upload_to="posts")
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True, default=None)
     content = models.TextField(validators=[MinLengthValidator(10)])
--- my_site/settings.py
+++ my_site/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "my_site" / "static"
 ]
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/"
