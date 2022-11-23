775b35ae9e0f5cef36371b24d182190eca054d8f
Suraj Atkari <surajatkari@yahoo.com>
2022-11-23 17:44:46 +0530

    added Comment model in models.py

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,14 @@ class Tag(models.Model):
         return f"{self.caption}"
 
 
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField(max_length=500)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
