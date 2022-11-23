7dfda053a1f1f9e2d67e1b06ec51bdc8f60cc3ae
Suraj Atkari <surajatkari@yahoo.com>
2022-11-23 15:16:48 +0530

    from django.confi.urls.static import static

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
